eade856f00b84919d9fbca5072c0d6c418ebebc8
HOT Quality <timobrownng@gmail.com>
2017-10-27 01:41:12 +0100

    Added tests for GET /users/me and POST /users

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,6 +3,7 @@ const request = require("supertest");
 const {ObjectID} = require("mongodb");
 
 const {app} = require("./../server");
+const {User} = require("./../models/user");
 const {Todo} = require("./../models/todo");
 const {todos, populateTodos, users, populateUsers} = require("./seed/seed");
 
@@ -171,6 +172,82 @@ describe("PATCH /todos/:id", () => {
         // expect(res.body.todo.completedAt).toBeNull(); // another option
         expect(res.body.todo.completedAt).toBeFalsy();
       })
+      .end(done);
+  });
+});
+
+describe("GET /users/me", () => {
+  it("should return user if authenticated", (done) => {
+    request(app)
+      .get("/users/me")
+      .set("x-auth", users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
+  });
+
+  it("should return 401 if not authenticated", (done) => {
+    request(app)
+      .get("/users/me")
+      .expect(401)
+      .expect((res) => {
+        expect(res.body).toEqual({});
+      })
+      .end(done);
+
+  });
+});
+
+describe("POST /users", () => {
+  it("should create a user using valid data", (done) => {
+    var email = "example@example.com";
+    var password = "samplePass123";
+
+    request(app)
+      .post("/users")
+      .send({email, password})
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers["x-auth"]).toBeDefined();
+        expect(res.body._id).toBeDefined();
+        expect(res.body.email).toBe(email);
+      })
+      .end((err) => {
+        if(err) {
+          return done(err);
+        }
+
+        User.findOne({email}).then((user) => {
+          expect(user).toBeDefined();
+          expect(user.password).not.toBe(password);
+          done();
+        });
+      });
+  });
+
+  it("should return validation errors using invalid data", (done) => {
+    var email = "info@example";
+    var password = "abc";
+
+    request(app)
+      .post("/users")
+      .send({email, password})
+      .expect(400)
+      .end(done);
+  });
+
+  it("should not create user if email in use", (done) => {
+    //var email = users[0].email;
+    var email = "timobrown@test.com";
+    var password = "userOnePass";
+
+    request(app)
+      .post("/users")
+      .send({email, password})
+      .expect(400)
       .end(done);
   });
 });
